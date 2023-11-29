df5fa605b1285a34c6cbee3312f35866e54a1c63
Lucas Liachi <lucas.liachi@gmail.com>
2023-11-28 23:03:37 -0300

    exercicio algebra e arvore de decisao

--- Data collection/nba_teams.py
+++ Data collection/nba_teams.py
@@ -1,31 +1,17 @@
-from nba_api.stats.endpoints._base import Endpoint
-from nba_api.stats.library.http import NBAStatsHTTP
+from requests import get
 
-class TeamDetails(Endpoint):
-    # ... (the rest of the class implementation as provided in the original code)
+BASE_URL = "http://data.nba.net"
+ALL_JSON = "/prod/v1/today.json"
 
-    # Function to get team details for a list of team IDs
-    def get_team_details_for_ids(team_ids):
-        team_details_list = []
+def get_links():
+    response = get(BASE_URL+ALL_JSON).json()
+    return response["links"]
 
-        for team_id in team_ids:
-            team_details = TeamDetails(team_id)
-            team_details_list.append(team_details)
+def get_currentScoreboard():
+    response = get(BASE_URL+get_links()["currentScoreboard"]).json()
+    games = response.keys("games")
 
-        return team_details_list
+    for game in games:
+        print(game.keys())
 
-# Example usage
-if __name__ == "__main__":
-    # Replace with actual team IDs
-    team_ids = [1610612759, 1610612744, 1610612761, 1610612755]
-
-    # Get team details for the list of team IDs
-    team_details_list = TeamDetails.get_team_details_for_ids(team_ids)
-
-    # Display team information
-    for team_details in team_details_list:
-        print("Team Background for Team ID", team_details.parameters["TeamID"])
-        print("Team Name:", team_details.team_background.get_dict()["NICKNAME"])
-        print("City:", team_details.team_background.get_dict()["CITY"])
-        print("Arena:", team_details.team_background.get_dict()["ARENA"])
-        print("------------------------------")
+print(get_currentScoreboard())
